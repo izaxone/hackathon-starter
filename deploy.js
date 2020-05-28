c8471abb0198c7ef88a77d35c595085e6e33906c
Kevin Isham <5745349-izaxone@users.noreply.gitlab.com>
2020-05-28 16:00:15 -0600

    Added key

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '52.91.135.134',
       username: 'ubuntu',
-      privateKey: '../hs-key.pem'
+      privateKey: 'hs-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
